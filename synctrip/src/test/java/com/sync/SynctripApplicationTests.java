d5a843ae20fa82ed2e71dc60b970df759b383a40
KING-WS <rladntjd1193@gmail.com>
2026-04-21 09:39:10 +0900

    init

---
+++ synctrip/src/test/java/com/sync/SynctripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynctripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
