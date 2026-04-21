5db18065fae3b1f42c86a99840f3fcfea19b953f
KING-WS <rladntjd1193@gmail.com>
2026-04-21 10:15:54 +0900

    Docker + actions init

--- synctrip/src/test/java/com/sync/SynctripApplicationTests.java
+++ synctrip/src/test/java/com/sync/SynctripApplicationTests.java
@@ -3,7 +3,7 @@ package com.sync;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.ai.openai.api-key=test-key-for-ci")
 class SynctripApplicationTests {
 
     @Test
