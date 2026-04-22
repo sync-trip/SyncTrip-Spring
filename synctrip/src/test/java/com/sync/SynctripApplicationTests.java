58ff4aada5e91c8348e958bb3d717a01858b3d77
KING-WS <132835424+KING-WS@users.noreply.github.com>
2026-04-22 22:35:34 +0900

    Dev (#1)
    * init
    
    * Docker + actions init
    
    
    * CI / CD 수정

--- synctrip/src/test/java/com/sync/SynctripApplicationTests.java
+++ synctrip/src/test/java/com/sync/SynctripApplicationTests.java
@@ -2,8 +2,10 @@ package com.sync;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SynctripApplicationTests {
 
     @Test
