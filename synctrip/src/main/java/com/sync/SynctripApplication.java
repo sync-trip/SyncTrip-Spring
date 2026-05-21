333830399c2fae1eb48ed7e4d3cfdd5e0869402c
Min <kineuns@gmail.com>
2026-05-21 13:03:04 +0900

    첫 방 생성 도시 검색 중복 호출 방지 Spring 인메모리 캐싱 기능 추가- 0521 kmw

--- synctrip/src/main/java/com/sync/SynctripApplication.java
+++ synctrip/src/main/java/com/sync/SynctripApplication.java
@@ -3,9 +3,11 @@ package com.sync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.sync.config")
+@EnableCaching
 public class SynctripApplication {
 
     public static void main(String[] args) {
